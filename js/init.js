69ebf5649d2bbbfaa694d759be592e764a227f58
Aira <1596356870@qq.com>
2022-02-23 20:43:12 +0800

    文案修改、JS修改

--- js/init.js
+++ js/init.js
@@ -6,3 +6,23 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var kawaiiconsole = String.raw
+`
+✦ あおの星、見つけよう！ ✦
+
+  ____     ______  __     ______   ____     __  __  ____       
+ /\  _ \  /\  _  \/\ \   /\__  _\ /\  _ \  /\ \/\ \/\  _ \     
+ \ \ \/   \ \ \/\ \ \ \  \/_/\ \/ \ \ \/\ \\ \ \ \ \ \ \/\ \   
+  \ \ \    \ \  __ \ \ \    \ \ \  \ \  _ < \ \ \ \ \ \    /   
+   \ \ \_\ \\ \ \/\ \ \ \___\\_\ \__\ \ \/\ \\ \ \_\ \ \ \\ \  
+    \ \____/ \ \_\ \_\ \____//\_____\\ \____/ \ \_____\ \_\ \_\
+     \/___/   \/_/\/_/\/___/ \/_____/ \/___/   \/_____/\/_/\/ /
+
+       ✧ Calibur Pixel Website ✧
+        - Designed by Aira in Shanghai # KAWAII FOREVER #
+         - Powered by MDUI and Materalize
+`
+
+
+console.log('%c%s', 'color: #fb7299', kawaiiconsole);
